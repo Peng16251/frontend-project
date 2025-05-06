4ffba370d09640af63be7d8d63491b9d2a490c6c
penguin <twcode3456@gmail.com>
2025-05-06 17:11:03 +0800

    feat: 224 done

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
-<template></template>
+<template>
+  <TheIcon icon="home"></TheIcon>
+</template>
 
 <script setup lang="ts">
 import "./assets/base.css";
+import TheIcon from "./components/TheIcon.vue";
 </script>
 
 <style scoped></style>
