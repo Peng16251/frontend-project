ae60775321e9f894cdc25b0de76f4e14be697c4f
penguin <twcode3456@gmail.com>
2025-05-08 05:00:21 +0800

    feat: 242 done

---
+++ src/utils/request.js
@@ -0,0 +1,17 @@
+﻿import { getJwtToken } from "./jwt.js";
+export async function request(
+	url,
+	{ method = "GET", body, headers, auth = true } = {},
+) {
+	const res = await fetch(url, {
+		method,
+		headers: {
+			"Content-Type": "application/json",
+			...(auth && { Authorization: `Bearer ${getJwtToken()}` }),
+			...headers,
+		},
+		...(body && { body: JSON.stringify(body) }),
+	});
+	const result = await res.json();
+	return result;
+}
