2c521696b63c221994b51d1e5e82f6fde7a7508e
penguin <twcode3456@gmail.com>
2025-05-08 14:10:07 +0800

    feat: 248 done

---
+++ src/apis/post.js
@@ -0,0 +1,15 @@
+﻿import { getJwtToken, getUser } from "./auth";
+import { request } from "../utils/request";
+export async function createPost(image, description) {
+    const formData = new FormData(); // strapi 接收 formdata 格式來處理 image
+    formData.append("files.image", image); // strapi => keys 為 files.image value 為 image
+    formData.append("data", JSON.stringify({ description })); // strapi => 其他的存為 data
+
+    await fetch("/api/posts", {
+        method: "POST",
+        body: formData,
+        headers: {
+            Authorization: `Bearer ${getJwtToken()}`,
+        },
+    });
+}
