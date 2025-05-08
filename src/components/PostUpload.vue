a764f9205c09a7a886a88d658fef0bab7d95ddba
penguin <twcode3456@gmail.com>
2025-05-08 13:45:36 +0800

    feat: 247 done

--- src/components/PostUpload.vue
+++ src/components/PostUpload.vue
@@ -1,9 +1,15 @@
 ﻿<template>
-  <TheModal>
+  <TheModal @close="store.commit('changeShowPostUpload', false)">
     <div class="postUpload">
       <label class="upload">
-        <TheIcon icon="upload-image" />
-        <input type="file" accept="image/*" class="fileChooser" />
+        <img v-if="imageObjUrl" :src="imageObjUrl" class="preview" />
+        <TheIcon v-else icon="upload-image" />
+        <input
+          type="file"
+          accept="image/*"
+          class="fileChooser"
+          @change="handleImageUpload"
+        />
       </label>
       <div class="postContent">
         <textarea
@@ -17,10 +23,21 @@
 </template>
 <script setup>
 import { ref } from "vue";
-// import { useStore } from "vuex";
 import TheButton from "./TheButton.vue";
+``;
 import TheIcon from "./TheIcon.vue";
 import TheModal from "./TheModal.vue";
+import { useStore } from "vuex";
+
+const store = useStore();
+const imageObjUrl = ref("");
+
+async function handleImageUpload(event) {
+  const imageFile = event.target.files[0];
+  if (imageFile) {
+    imageObjUrl.value = URL.createObjectURL(imageFile);
+  }
+}
 </script>
 <style scoped>
 .postUpload {
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -5,12 +5,19 @@
     </PostList>
 
     <!-- <PostDetails /> -->
-    <!-- <PostUpload /> -->
+    <PostUpload v-if="showPostUpload" />
   </div>
 </template>
 <script setup lang="ts">
+import { computed } from "vue";
 import PostItem from "../components/PostItem.vue";
 import PostList from "../components/PostList.vue";
 import PostUpload from "../components/PostUpload.vue";
+import { useStore } from "vuex";
+
+const store = useStore();
+const showPostUpload = computed(() => {
+  return store.state.showPostUpload;
+});
 </script>
 <style scoped></style>
